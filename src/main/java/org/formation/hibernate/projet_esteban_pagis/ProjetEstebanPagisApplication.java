11d2c75b5ddf2eeefaea764bc5f7743eadb404d5
Esteban Pagis <esteban.pagis@epita.fr>
2025-11-20 10:40:53 +0100

    Initial commit

---
+++ src/main/java/org/formation/hibernate/projet_esteban_pagis/ProjetEstebanPagisApplication.java
@@ -0,0 +1,13 @@
+package org.formation.hibernate.projet_esteban_pagis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEstebanPagisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEstebanPagisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/formation/hibernate/projet_esteban_pagis/ProjetEstebanPagisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.hibernate.projet_esteban_pagis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEstebanPagisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
